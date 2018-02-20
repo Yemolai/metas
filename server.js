121c24c3cd4423600f4b398117c060fda6769d18
Romulo Gabriel Rodrigues <yemolai@gmail.com>
2018-02-20 09:25:33 -0300

    Enhanced package information file

--- server.js
+++ server.js
@@ -4,24 +4,40 @@ const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
 const db = require('./models/');
 
-const typeDefs = [`
-type Usuario {
-  id: ID!
-  guid: String
-  matricula: String
-  nome: String
-  usuario: String
-  senha: String
-}
+const channels = [{
+  id: 1,
+  name: 'soccer',
+}, {
+  id: 2,
+  name: 'baseball',
+}];
 
-type Query {
-  hello: String,
-  usuarios: [Usuario]
-}
+let nextId = 3;
 
-schema {
-  query: Query
-}`];
+const typeDefs = [
+  `type Channel {
+      id: ID! # "!" denotes a required field
+      name: String
+      messages: [Message]!
+    }
+
+    type Message {
+      id: ID!
+      text: String
+    }
+    # This type specifies the entry points into our API. 
+    type Query {
+      channels: [Channel]    # "[]" means this is a list of channels
+      channel(id: ID!): Channel
+    }
+
+    # The mutation root type, used to define all mutations.
+    type Mutation {
+      # A mutation to add a new channel to the list of channels
+      addChannel(name: String!): Channel
+    }
+  `
+];
 
 const resolvers = {
   Query: {
@@ -35,4 +51,7 @@ const schema = makeExecutableSchema({typeDefs, resolvers});
 const app = express();
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
 app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+app.use('/db', function (req, res, next) {
+
+})
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
